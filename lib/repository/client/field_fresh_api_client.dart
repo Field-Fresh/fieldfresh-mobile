eaa59e6fc8bdec4055843e2df47f7145777c04c5
Mitheysh Asokan <mitheyshasokan@Mitheyshs-MacBook-Pro-2.local>
2021-02-03 18:01:24 -0500

    Fixed ios localhost issues.

--- lib/repository/client/field_fresh_api_client.dart
+++ lib/repository/client/field_fresh_api_client.dart
@@ -1,17 +1,18 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
 class FieldFreshApi {
   final http.Client httpClient;
 
-  final String baseURL = "10.0.2.2:9090";
+  final String baseURL =
+      Platform.isAndroid ? "10.0.2.2:9090" : "localhost:9090";
 
   final Map<String, String> basePostHeader = {
     "Content-Type": "application/json",
   };
 
-  FieldFreshApi({httpClient,}): this.httpClient = httpClient ?? http.Client();
-
-
-
-
+  FieldFreshApi({
+    httpClient,
+  }) : this.httpClient = httpClient ?? http.Client();
 }
