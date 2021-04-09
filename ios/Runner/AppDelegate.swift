34f70cee6e90433ab01a535d22227098278a357a
Mitheysh Asokan <mitheyshasokan@Mitheyshs-MacBook-Pro-2.local>
2021-04-09 11:45:20 -0400

    Integrated with Ashkan's changes, and flavors for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
-import GoogleMaps
+
+let kChannel = "flavor"
+let kMethodFlavor = "getFlavor"
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +10,17 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    
+    GeneratedPluginRegistrant.register(with:self)
+    guard let controller = self.window.rootViewController as? FlutterViewController else {return true}
+    let flavorChannel = FlutterMethodChannel(name: kChannel, binaryMessenger: controller as! FlutterBinaryMessenger)
+    flavorChannel.setMethodCallHandler{ (call, result) in
+        if call.method == kMethodFlavor{
+            let flavor = Bundle.main.object(forInfoDictionaryKey: "Flavor") as! String
+            result(flavor)
+        }
+        
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
